05b166d320fec537837dff1ef9fe51ff64e46d2a
rdrggrgld <rodrigogregoldo@gmail.com>
2020-06-07 11:14:28 -0300

    Landing page ok, roteada para pagina de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// a extensão é .tsx, pois esse arquivo permite mesclar sintaxes de JavaScript com XML (HTML)
+// JSX: JavaScript XML.
+
+import Routes from './routes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <Routes />
+   </div>
   );
 }
 
