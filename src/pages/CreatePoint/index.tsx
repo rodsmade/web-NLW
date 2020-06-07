fcf0c7655b9c9158f276d09dd04ca64aa6edf36a
rdrggrgld <rodrigogregoldo@gmail.com>
2020-06-07 12:25:33 -0300

    Inseri lib para ler a API do back, e no html da pagina de cadastro implementei a busca de todos os itens de coleta na minha api

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { TileLayer, Marker, Map } from 'react-leaflet'
@@ -8,7 +8,30 @@ import './styles.css'
 
 import logo from '../../assets/logo.svg'
 
+interface Item{
+    id: number;
+    titulo: string;
+    url_imagem: string;
+}
+
 const CreatePoint = () => {
+
+    /* ATENSSAU 
+    Sempre que criar um estado para um array ou objeto, é necessário manualmente informar
+    o tipo da variável que vai ser armazenada ali dentro. 
+    Para isso, crio uma interface (acima) e implemento na função useState o generic com a interface
+    */
+    const [items, setItems] = useState<Item[]>([]);// um estado, +- global var. Importante ser declarada right after component! 
+
+    useEffect(() => {
+        api.get('itens')
+            .then(response => {
+                setItems(response.data) // response só existe dentro dessa função. "variáveis globais" nesse caso são estados.
+            })
+    }, [])  /* conceito de Estado do JS: o colchetes recebe o gatilho para atualizar o estado do useEffect.
+            Vazio significa que independente de qqr alteração no componente CreatePoint,
+            o q tá dentro das chaves vai ser carregado uma única vez, assim q a página for carregada.
+            */
     return(
         <div id="page-create-point">
             <header>
@@ -93,30 +116,12 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="lâmpadas"/>
-                            <span>Lâmpadas</span>
+                        {items.map(item => (
+                            <li key={item.id} /*Each child in a list should have a unique "key" prop.*/>  
+                            <img src={item.url_imagem} alt={item.titulo}/>
+                            <span>{item.titulo}</span>
                         </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="lâmpadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
+                        ))}
                     </ul>
 
                 </fieldset>
