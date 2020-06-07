47a24a8b97c3685cbf7c8743548a1e72efe7be4c
rdrggrgld <rodrigogregoldo@gmail.com>
2020-06-07 13:51:26 -0300

    Criados estados para guardar selecao de estado e cidade, e opcao pro usuario selecionar uma localidade no mapa

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, ChangeEvent } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { TileLayer, Marker, Map } from 'react-leaflet'
 import api from '../../services/api'
 import axios from 'axios'
+import { LeafletMouseEvent } from 'leaflet'
 
 import './styles.css'
 
@@ -21,6 +22,10 @@ interface IBGEUFResponse{
     nome: string;
 }
 
+interface IBGECidadeResponse{
+    nome: string;
+}
+
 const CreatePoint = () => {
 
     /* ATENSSAU 
@@ -30,6 +35,10 @@ const CreatePoint = () => {
     */
     const [items, setItems] = useState<Item[]>([]); // um estado, +- global var. Importante ser declarada right after component! 
     const [ufs, setUfs] = useState<string[]>([]); // veio de um obj. IBGEUFResponse, mas dps de filtrar é só um array de strings
+    const [cidades, setCidades] = useState<string[]>([]);
+    const [ufSelecionada, setUfSelecionada] = useState('0');
+    const [cidadeSelecionada, setCidadeSelecionada] = useState('0');
+    const [coordenadaSelecionada, sertCoordenadaSelecionada] = useState<[number, number]>([0,0]);
 
     useEffect(() => {
         api.get('itens')
@@ -40,7 +49,6 @@ const CreatePoint = () => {
             Vazio significa que independente de qqr alteração no componente CreatePoint,
             o q tá dentro das chaves vai ser carregado uma única vez, assim q a página for carregada.
             */
-
     useEffect(() => {
     axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
         .then(response => {
@@ -48,6 +56,36 @@ const CreatePoint = () => {
             setUfs(siglas);
         })
     }, [])
+    useEffect(() => {
+        // carregar as cidades sempre que a UF muda
+        if (ufSelecionada === '0'){
+            return;
+        }
+        
+        axios
+            .get<IBGEUFResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufSelecionada}/municipios`)
+            .then(response => {
+                const cidades = response.data.map(c => c.nome); // meio que filtrei a response, só me interessam as siglas
+                setCidades(cidades);
+            })
+    }, [ufSelecionada])
+
+    function guardaUFSelecionado(evento: ChangeEvent<HTMLSelectElement>){
+        const ufSelecionada = evento.target.value;
+
+        setUfSelecionada(ufSelecionada);
+    }
+    function guardaCidadeSelecionada(evento: ChangeEvent<HTMLSelectElement>){
+        const cidadeSelecionada = evento.target.value;
+
+        setCidadeSelecionada(cidadeSelecionada);
+    }
+    function guardaCoordenadasDoMapa(evento: LeafletMouseEvent){    
+        sertCoordenadaSelecionada([
+            evento.latlng.lat,
+            evento.latlng.lng
+        ]);
+    }
 
     return(
         <div id="page-create-point">
@@ -101,29 +139,42 @@ const CreatePoint = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
-                    <Map center={[-23.5468342,-46.6117453]} zoom={15}>
+                    <Map center={[-23.5468342,-46.6117453]} zoom={15} onClick={guardaCoordenadasDoMapa}>
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                        <Marker position={[-23.5468342,-46.6117453]}/>
+                        <Marker position={coordenadaSelecionada}/>
                     </Map>
 
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
-                            <select name="uf" id="uf">
+                            <select 
+                                name="uf"
+                                id="uf" 
+                                value={ufSelecionada} 
+                                onChange={guardaUFSelecionado}
+                            >
                                 <option value="0">Selecione uma UF</option>
                                 {ufs.map(sigla => (
-                                    <option key ={sigla}value={sigla}>{sigla}</option>
+                                    <option key={sigla} value={sigla}>{sigla}</option>
                                 ))}
                             </select>
                         </div>
 
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city">
+                            <select
+                                name="city"
+                                id="city"
+                                value={cidadeSelecionada}
+                                onChange={guardaCidadeSelecionada}
+                            >
                                 <option value="0">Selecione uma cidade</option>
+                                {cidades.map(cidade => (
+                                    <option key={cidade} value={cidade}>{cidade}</option>
+                                ))}
                             </select>
                         </div>
                     </div>
